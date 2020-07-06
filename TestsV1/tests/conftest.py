ceec66e547c3fd38322e8512e075adf69f53217f
StephaneColson <stephane.colson@protonmail.com>
2020-07-06 14:46:45 +0200

    No assertions logged by default in traditional-report.txt

--- TestsV1/tests/conftest.py
+++ TestsV1/tests/conftest.py
@@ -69,14 +69,17 @@ def logTaskReport(writer, py_config, test_case, item):
         "h": item.funcargs["height"] if "height" in item.fixturenames else "",
         "display": getDisplayType(item),
     }
-    assertionError = ""
-    if item.rep_call.longrepr is not None and item.rep_call.longrepr.reprcrash is not None:
-        assertionError = item.rep_call.longrepr.reprcrash.message
+    # assertionError = ""
+    # if item.rep_call.longrepr is not None and item.rep_call.longrepr.reprcrash is not None:
+    #     assertionError = item.rep_call.longrepr.reprcrash.message
 
     domId = item.funcargs["location"] if "location" in item.fixturenames else None
 
+    # reportMessage = f"Task: {taskNumber}, Test Name: {testName}, DOM Id: {domId}, Browser: {browserName}, " \
+    #                 f"Viewport: {p['w']}x{p['h']}, Device: {p['display']}, Status: {testResult} ({assertionError})"
+
     reportMessage = f"Task: {taskNumber}, Test Name: {testName}, DOM Id: {domId}, Browser: {browserName}, " \
-                    f"Viewport: {p['w']}x{p['h']}, Device: {p['display']}, Status: {testResult} ({assertionError})"
+                    f"Viewport: {p['w']}x{p['h']}, Device: {p['display']}, Status: {testResult}"
 
     log(reportMessage)
 
