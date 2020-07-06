26f185bdec07641c8b189aee3a401509553ad097
StephaneColson <stephane.colson@protonmail.com>
2020-07-06 13:07:09 +0200

    Rework pageObject

--- pages/HomePage.py
+++ pages/HomePage.py
@@ -1,73 +1,54 @@
-# from pylenium import Pylenium
-# from pylenium.element import Element
+from pages.GlobalPage import GlobalPageMap, GlobalPage
 
 
-class HomePageMap:
-    SEARCH_FIELD = '#INPUTtext____42'
-    WISHLIST_ICON = '#A__wishlist__52'
+class HomePageMap(GlobalPageMap):
     FILTER_COLUMN = '#filter_col'
     TOP_BANNER_IMAGE = '.top_banner'
     PRODUCT_GRID = '#product_grid'
+    PRODUCT_1 = '#product_1'
+    PRODUCT_8 = '#product_8'
     FILTER_COLOR_BLACK = '#colors__Black'
     FILTER_BUTTON = '#filterBtn'
     LEFT_SIDE_FILTER_ICON = '#ti-filter'
-    SMALL_REFERENCE_ID = '#SMALL____84'
-    NICE_SELECT_SIZE = '.nice-select'
-    NEW_PRICE_LABEL = "#new_price"
-    PROFILE_ICON = ".access_link"
-    CART_BUTTON = "#A__cartbt__49"
-    FIFTH_STAR = "#I__iconstar__81"
-    RATING_COUNT = "#EM____82"  # For V1
-    # RATING_COUNT = "#EM__ratingcoun__82" # For V2
-    DEFAULT_QUANTITY = "#quantity_1"
-    BUTTON_ADD_TO_CART = "#A__btn__114"
-    SHOE_IMAGE = "#shoe_img"
 
 
-class AppliFashionHomePage:
-    def __init__(self, py, width, height, URL):
+class HomePage(GlobalPage):
+    def __init__(self, py, width, height, baseUrl):
         self.py = py
         self.py.viewport(width, height, 'portrait')
-        self.py.visit(URL)
+        self.py.visit(baseUrl)
         self.domId = ''
         self.filterColorDomId = ''
         self.filterButton = ''
         self.firstProduct = ''
         self.map = {
-            HomePageMap.SEARCH_FIELD: lambda: self.py.get(HomePageMap.SEARCH_FIELD),
-            HomePageMap.WISHLIST_ICON: lambda: self.py.get(HomePageMap.WISHLIST_ICON),
             HomePageMap.FILTER_COLUMN: lambda: self.py.get(HomePageMap.FILTER_COLUMN),
             HomePageMap.TOP_BANNER_IMAGE: lambda: self.py.get(HomePageMap.TOP_BANNER_IMAGE),
             HomePageMap.PRODUCT_GRID: lambda: self.py.get(HomePageMap.PRODUCT_GRID),
+            HomePageMap.PRODUCT_1: lambda: self.py.get(HomePageMap.PRODUCT_1),
+            HomePageMap.PRODUCT_8: lambda: self.py.get(HomePageMap.PRODUCT_8),
             HomePageMap.FILTER_COLOR_BLACK: lambda: self.py.get(HomePageMap.FILTER_COLOR_BLACK),
             HomePageMap.FILTER_BUTTON: lambda: self.py.get(HomePageMap.FILTER_BUTTON),
             HomePageMap.LEFT_SIDE_FILTER_ICON: lambda: self.py.get(HomePageMap.LEFT_SIDE_FILTER_ICON),
-            HomePageMap.SMALL_REFERENCE_ID: lambda: self.py.get(HomePageMap.SMALL_REFERENCE_ID),
-            HomePageMap.NICE_SELECT_SIZE: lambda: self.py.get(HomePageMap.NICE_SELECT_SIZE),
-            HomePageMap.NEW_PRICE_LABEL: lambda: self.py.get(HomePageMap.NEW_PRICE_LABEL),
-            HomePageMap.PROFILE_ICON: lambda: self.py.get(HomePageMap.PROFILE_ICON),
-            HomePageMap.CART_BUTTON: lambda: self.py.get(HomePageMap.CART_BUTTON),
-            HomePageMap.FIFTH_STAR: lambda: self.py.get(HomePageMap.FIFTH_STAR),
-            HomePageMap.RATING_COUNT: lambda: self.py.get(HomePageMap.RATING_COUNT),
-            HomePageMap.DEFAULT_QUANTITY: lambda: self.py.get(HomePageMap.DEFAULT_QUANTITY),
-            HomePageMap.BUTTON_ADD_TO_CART: lambda: self.py.get(HomePageMap.BUTTON_ADD_TO_CART),
-            HomePageMap.SHOE_IMAGE: lambda: self.py.get(HomePageMap.SHOE_IMAGE),
         }
 
     def __find(self, path):
         return self.map[path]() if path in self.map.keys() else None
-
-    def getSearchField(self):
-        return self.__find(HomePageMap.SEARCH_FIELD)
-
-    def getWishlistIcon(self):
-        return self.__find(HomePageMap.WISHLIST_ICON)
 
     def getFilterColumn(self):
         return self.__find(HomePageMap.FILTER_COLUMN)
 
     def getTopBannerImage(self):
         return self.__find(HomePageMap.TOP_BANNER_IMAGE)
+
+    def openLeftSideFilter(self):
+        self.__find(HomePageMap.LEFT_SIDE_FILTER_ICON).click()
+
+    def getProduct_1(self):
+        return self.__find(HomePageMap.PRODUCT_1)
+
+    def getProduct_8(self):
+        return self.__find(HomePageMap.PRODUCT_8)
 
     def getProductGrid(self):
         return self.__find(HomePageMap.PRODUCT_GRID)
@@ -87,49 +68,8 @@ class AppliFashionHomePage:
     def gotoFirstBlackColorProduct(self):
         self.getBlackColorProducts().children().first().click()
 
-    def getReference(self):
-        return self.__find(HomePageMap.SMALL_REFERENCE_ID)
-
     def getFilterButton(self):
         return self.__find(HomePageMap.FILTER_BUTTON)
 
-    def openLeftSideFilter(self):
-        self.__find(HomePageMap.LEFT_SIDE_FILTER_ICON).click()
 
-    def getDefaultSize(self):
-        return self.__find(HomePageMap.NICE_SELECT_SIZE)
-
-    def getPrize(self):
-        return self.__find(HomePageMap.NEW_PRICE_LABEL).text()
-
-    def checkTopToolsIconsNotOverlapping(self):
-        profileIcon = self.__find(HomePageMap.PROFILE_ICON)
-        wishListIcon = self.__find(HomePageMap.WISHLIST_ICON)
-        cartButton = self.__find(HomePageMap.CART_BUTTON)
-
-        rectProfileIcon = profileIcon.webelement.rect
-        rectWishListIcon = wishListIcon.webelement.rect
-        rectCartButton = cartButton.webelement.rect
-
-        # Explain: check that right position of icon n < left position of icon n+1
-        return (((rectProfileIcon["x"] + rectProfileIcon["width"]) <= rectWishListIcon["x"]) and
-                ((rectWishListIcon["x"] + rectWishListIcon["width"]) <= rectCartButton["x"]))
-
-    def checkRatingReviewOverlapping(self):
-        fifthStar = self.__find(HomePageMap.FIFTH_STAR)
-        ratingCount = self.__find(HomePageMap.RATING_COUNT)
-
-        rectFifthStar = fifthStar.webelement.rect
-        rectRatingCount = ratingCount.webelement.rect
-
-        return (rectFifthStar["x"] + rectFifthStar["width"]) <= rectRatingCount["x"]
-
-    def getDefaultQuantity(self):
-        return self.__find(HomePageMap.DEFAULT_QUANTITY).get_attribute('value')
-
-    def getButtonAddToCart(self):
-        return self.__find(HomePageMap.BUTTON_ADD_TO_CART)
-
-    def getShoeImage(self):
-        return self.__find(HomePageMap.SHOE_IMAGE).get_attribute('style')
 
--- tests/TraditionalTestsV1.py
+++ tests/TraditionalTestsV1.py
@@ -1,11 +1,15 @@
 import pytest, re
 
-from pages.HomePage import AppliFashionHomePage, HomePageMap
+from pages.GlobalPage import GlobalPageMap, GlobalPage
+from pages.HomePage import HomePageMap, HomePage
+from pages.ProductDetailPage import ProductDetailPageMap, ProductDetailPage
 
 MOBILE = 0
 TABLET = 1
 DESKTOP = 2
 
+backShoe1Page = '?id=1'
+backShoe8Page = '?id=8'
 
 class TestData:
     # (Width, Height, type of display (0, 1, 2) and element path?)
@@ -23,12 +27,12 @@ class TestData:
 
 # Check that search field is displayed on tablet and desktop, hidden on mobile
 @pytest.mark.parametrize("width,height,displayType,location,testName",
-                         TestData.forElement(HomePageMap.SEARCH_FIELD,
+                         TestData.forElement(GlobalPageMap.SEARCH_FIELD,
                                              'Search field is displayed on tablet and desktop'))
 @pytest.mark.task1
 def test_search_field_task1(py, report_generator, width, height, displayType, location, testName):
-    homePage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
-    searchField = homePage.getSearchField()
+    globalPage = GlobalPage(py, width, height, py.config.custom['environment']['homeUrl'])
+    searchField = globalPage.getSearchField()
 
     # Check that search field is hidden for mobile display only
     if displayType == MOBILE:
@@ -39,12 +43,12 @@ def test_search_field_task1(py, report_generator, width, height, displayType, lo
 
 # Check that wishlist icon (heart) is only displayed on Desktop
 @pytest.mark.parametrize("width,height,displayType,location,testName",
-                         TestData.forElement(HomePageMap.WISHLIST_ICON,
+                         TestData.forElement(GlobalPageMap.WISHLIST_ICON,
                                              'Wishlist icon is displayed on desktop only'))
 @pytest.mark.task1
 def test_wishlist_icon_task1(py, report_generator, width, height, displayType, location, testName):
-    homePage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
-    wishlistIcon = homePage.getWishlistIcon()
+    globalPage = GlobalPage(py, width, height, py.config.custom['environment']['homeUrl'])
+    wishlistIcon = globalPage.getWishlistIcon()
 
     # Check that wishlist icon is hidden for mobile and tablet display
     if displayType == DESKTOP:
@@ -59,7 +63,7 @@ def test_wishlist_icon_task1(py, report_generator, width, height, displayType, l
                                              'Left filter column is displayed on desktop only'))
 @pytest.mark.task1
 def test_filter_column_task1(py, report_generator, width, height, displayType, location, testName):
-    homePage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
+    homePage = HomePage(py, width, height, py.config.custom['environment']['homeUrl'])
     filterColumn = homePage.getFilterColumn()
 
     if displayType == DESKTOP:
@@ -74,7 +78,7 @@ def test_filter_column_task1(py, report_generator, width, height, displayType, l
                                              'Div top banner is always displayed'))
 @pytest.mark.task1
 def test_top_banner_task1(py, report_generator, width, height, displayType, location, testName):
-    homePage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
+    homePage = HomePage(py, width, height, py.config.custom['environment']['homeUrl'])
     topBannerImage = homePage.getTopBannerImage()
 
     # Check that topBannerImage is always visible
@@ -87,7 +91,7 @@ def test_top_banner_task1(py, report_generator, width, height, displayType, loca
                                              'Search black shoes and check results'))
 @pytest.mark.task2
 def test_blackShoes_filter_task2(py, report_generator, width, height, displayType, location, testName):
-    filterPage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
+    filterPage = HomePage(py, width, height, py.config.custom['environment']['homeUrl'])
 
     if displayType != DESKTOP:
         filterPage.openLeftSideFilter()
@@ -95,138 +99,126 @@ def test_blackShoes_filter_task2(py, report_generator, width, height, displayTyp
     productsFiltered = filterPage.getBlackColorProducts()
 
     # @TODO: work with developers in order to have color of results in the DOM
-    # Here, I just check that we only retrieve product_1 and product_8 and nothing else
-    # which might not be true in the real world where product list is changed
-    assert productsFiltered.children().length() == 2
+    # Here, I just check that the number of results is 2 and that
+    # we only retrieve product_1 and product_8 and nothing else
+    # which might not be true in the real world where product list is dynamic
+
+    NbElements = productsFiltered.children().length()
+    product1 = filterPage.getProduct_1()
+    product2 = filterPage.getProduct_8()
+
+    assert (NbElements == 2 and
+            product1.get_attribute('href') == py.config.custom['environment']['productDetailUrl'] + '?id=1' and
+            product2.get_attribute('href') == py.config.custom['environment']['productDetailUrl'] + '?id=8')
 
 
 # Check that reference of the product is visible
 @pytest.mark.parametrize("width,height,displayType,location,testName",
-                         TestData.forElement(HomePageMap.SMALL_REFERENCE_ID,
+                         TestData.forElement(ProductDetailPageMap.SMALL_REFERENCE_ID,
                                              'Check that reference of the product '
                                              'is visible in black shoes detail page'))
 @pytest.mark.task3
 def test_blackShoesDetail_reference_task3(py, report_generator, width, height, displayType, location, testName):
-    filterPage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
-    if displayType != DESKTOP:
-        filterPage.openLeftSideFilter()
-
-    filterPage.gotoFirstBlackColorProduct()
+    filterPage = ProductDetailPage(py, width, height, py.config.custom['environment']['productDetailUrl'], backShoe1Page)
 
     # "displayed" means that the element is in the DOM and has a size
     #  greater than zero such that it is visible to the user.
-    # @Todo: so it returns true even if element is not visible (white on white) :/
-    assert filterPage.getReference().is_displayed()
+    # @Todo: so it returns true even if element is not visible (white on white).
+    #  getCssValue not supported by pylenium :/
+    reference = filterPage.getReference()
+    # refWebElement = reference.webelement
+    # backgroundColor = refWebElement.getCssValue("background-color")
+    # color = refWebElement.getCssValue("color")
+    # print("Color: "+color)
+    # print("backgroundColor: " + backgroundColor)
+    assert (reference.is_displayed())
+    # and backgroundColor != color)
 
 
 # Check that default size is Small (S)
 # @Todo But maybe we should use a list of available sizes !?
 @pytest.mark.parametrize("width,height,displayType,location,testName",
-                         TestData.forElement(HomePageMap.NICE_SELECT_SIZE,
+                         TestData.forElement(ProductDetailPageMap.NICE_SELECT_SIZE,
                                              'Check that default size of the product is \'Small (S)\' '
                                              'in black shoe detail page'))
 @pytest.mark.task3
 def test_blackShoesDetail_defaultSize_task3(py, report_generator, width, height, displayType, location, testName):
-    filterPage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
-    if displayType != DESKTOP:
-        filterPage.openLeftSideFilter()
+    filterPage = ProductDetailPage(py, width, height, py.config.custom['environment']['productDetailUrl'], backShoe1Page)
 
-    filterPage.gotoFirstBlackColorProduct()
     filterPage.getDefaultSize().should().have_text('Small (S)')
 
 
 # Check that price is correctly displayed with decimals
 @pytest.mark.parametrize("width,height,displayType,location,testName",
-                         TestData.forElement(HomePageMap.NEW_PRICE_LABEL,
+                         TestData.forElement(ProductDetailPageMap.NEW_PRICE_LABEL,
                                              'Check that price of the product contains .00 in black shoe detail page'))
 @pytest.mark.task3
 def test_blackShoesDetail_price_task3(py, report_generator, width, height, displayType, location, testName):
-    filterPage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
-    if displayType != DESKTOP:
-        filterPage.openLeftSideFilter()
+    filterPage = ProductDetailPage(py, width, height, py.config.custom['environment']['productDetailUrl'], backShoe1Page)
 
-    filterPage.gotoFirstBlackColorProduct()
     # Using a regexp to check that price starts with $, then several digit, then a '.' and 2 more digits
     assert re.search("[$][0-9]+.[0-9]{2}", filterPage.getPrize()) is not None
 
 
 # Check that topTools icons (heart, profile and basket) don't overlap
 @pytest.mark.parametrize("width,height,displayType,location,testName",
-                         TestData.forElement(HomePageMap.PROFILE_ICON,
+                         TestData.forElement(GlobalPageMap.PROFILE_ICON,
                                              'Check that topTools icons (heart, profile and basket) '
                                              'don t overlap in black shoe detail page'))
 @pytest.mark.task3
 def test_blackShoesDetail_topToolsIcons_task3(py, report_generator, width, height, displayType, location, testName):
-    filterPage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
-    if displayType != DESKTOP:
-        filterPage.openLeftSideFilter()
+    filterPage = ProductDetailPage(py, width, height, py.config.custom['environment']['productDetailUrl'], backShoe1Page)
+    filterPage.useGlobalPageMap() # Elements Cart_Button, Profile_icon and Wishlist_icon are in GlobalPage
 
-    filterPage.gotoFirstBlackColorProduct()
     assert filterPage.checkTopToolsIconsNotOverlapping()
 
 
 # Check that rating stars and reviews are not overlapping
 @pytest.mark.parametrize("width,height,displayType,location,testName",
-                         TestData.forElement(HomePageMap.RATING_COUNT,
+                         TestData.forElement(ProductDetailPageMap.RATING_COUNT,
                                              'Check that rating stars and reviews '
                                              'are not overlapping in black shoe detail page'))
 @pytest.mark.task3
 def test_blackShoesDetail_ratingStarReviews_task3(py, report_generator, width, height, displayType, location, testName):
-    filterPage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
-    if displayType != DESKTOP:
-        filterPage.openLeftSideFilter()
+    filterPage = ProductDetailPage(py, width, height, py.config.custom['environment']['productDetailUrl'], backShoe1Page)
 
-    filterPage.gotoFirstBlackColorProduct()
-    # @Todo: ID has been changed from "#EM____82" (v1) to "#EM__ratingcoun__82" (v2), that's bad!
+    # @Todo: ID has been changed from "#EM____82" (v1) to "#EM__ratingcoun__82" (v2) :'(
     # Issue (33934) created : 3) Hackathon – App issue: Rating count ID has been changed between v1 and v2
-    # So we should comment/unncomment RATING_COUNT in HomePage depending on versioin tested
+    # Change ID in ProductDetailPageMap
     assert filterPage.checkRatingReviewOverlapping()
 
 
 # Check default quantity is 1
 @pytest.mark.parametrize("width,height,displayType,location,testName",
-                         TestData.forElement(HomePageMap.DEFAULT_QUANTITY,
+                         TestData.forElement(ProductDetailPageMap.DEFAULT_QUANTITY,
                                              'Check default quantity is 1 in black shoe detail page'))
 @pytest.mark.task3
 def test_blackShoesDetail_quantityAddToCart_task3(py, report_generator, width, height, displayType, location, testName):
-    filterPage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
-
-    if displayType != DESKTOP:
-        filterPage.openLeftSideFilter()
-    filterPage.gotoFirstBlackColorProduct()
+    filterPage = ProductDetailPage(py, width, height, py.config.custom['environment']['productDetailUrl'], backShoe1Page)
 
     assert filterPage.getDefaultQuantity() == '1'
 
 
 # Check that Add to Cart button is enabled by default
 @pytest.mark.parametrize("width,height,displayType,location,testName",
-                         TestData.forElement(HomePageMap.BUTTON_ADD_TO_CART,
+                         TestData.forElement(ProductDetailPageMap.BUTTON_ADD_TO_CART,
                                              'Check that Add to Cart button is enabled by default '
                                              'in black shoe detail page'))
 @pytest.mark.task3
 def test_blackShoesDetail_addToCardButton_task3(py, report_generator, width, height, displayType, location, testName):
-    filterPage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
-
-    if displayType != DESKTOP:
-        filterPage.openLeftSideFilter()
-    filterPage.gotoFirstBlackColorProduct()
+    filterPage = ProductDetailPage(py, width, height, py.config.custom['environment']['productDetailUrl'], backShoe1Page)
 
     assert filterPage.getButtonAddToCart().is_enabled()
 
 
 # Check product preview display
 @pytest.mark.parametrize("width,height,displayType,location,testName",
-                         TestData.forElement(HomePageMap.PRODUCT_GRID,
+                         TestData.forElement(ProductDetailPageMap.SHOE_IMAGE,
                                              'Check that product preview '
                                              'is displayed in black shoe detail page'))
 @pytest.mark.task3
 def test_blackShoesDetail_productPrevDisplay_task3(py, report_generator, width, height, displayType, location, testName):
-    filterPage = AppliFashionHomePage(py, width, height, py.config.custom['environment']['url'])
-
-    if displayType != DESKTOP:
-        filterPage.openLeftSideFilter()
-    filterPage.gotoFirstBlackColorProduct()
-
+    filterPage = ProductDetailPage(py, width, height, py.config.custom['environment']['productDetailUrl'], backShoe1Page)
     styleShoeImage = filterPage.getShoeImage()
-    print('Style: ' + styleShoeImage)
+
     assert 'url' in styleShoeImage
